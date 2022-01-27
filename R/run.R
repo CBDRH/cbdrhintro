12f4f0f10bd3d3de6cd3085dd3fee052788f0797
Mark Hanly <m.hanly@unsw.edu.au>
2022-01-27 20:39:35 +1100

    initial commit

---
+++ R/run.R
@@ -0,0 +1,30 @@
+#' run
+#'
+#' @param tute
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' run()
+run <- function(tute) {
+
+  # locate all the existing tutorials
+  validTutes <- list.files(system.file("tutorials", package = "cbdrhintro"))
+
+  validExamplesMsg <-
+    paste0(
+      "Available tutorials are: '",
+      paste(validTutes, collapse = "', '"),
+      "'")
+
+  # if an invalid example is given, throw an error
+  if (missing(tute) || !nzchar(tute) || !tute %in% validTutes) {
+    stop(
+      'Please specify a valid app as an argument.\n',
+      validExamplesMsg,
+      call. = FALSE)
+  }
+
+  learnr::run_tutorial(tute, package = "cbdrhintro")
+}
