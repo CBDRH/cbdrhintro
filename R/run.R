72163a550031d1934d1d305dfe83dd59a42d60c0
Mark Hanly <m.hanly@unsw.edu.au>
2022-02-03 10:42:42 +1100

    Removed run function entirely

--- R/run.R
+++ R/run.R
@@ -1,34 +1,3 @@
-#' Run an interactive learnr tutorial
-#'
-#' @param tute
-#'
-#' @return
-#'
-#' @examples
-#' run()
-run <- function(tute) {
-
-  # locate all the existing tutorials
-  validTutes <- list.files(system.file("tutorials", package = "cbdrhintro"))
-
-  validExamplesMsg <-
-    paste0(
-      "Available tutorials are: '",
-      paste(validTutes, collapse = "', '"),
-      "'")
-
-  # if an invalid example is given, throw an error
-  if (missing(tute) || !nzchar(tute) || !tute %in% validTutes) {
-    stop(
-      'Please specify a valid app as an argument.\n',
-      validExamplesMsg,
-      call. = FALSE)
-  }
-
-  learnr::run_tutorial(tute, package = "cbdrhintro")
-}
-
-
 #' Show an index of available tutorials
 #'
 #' @return
